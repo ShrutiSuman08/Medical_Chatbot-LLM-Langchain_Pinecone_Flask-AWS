d8ba75acefa8e682955e98e4322ff4e361f913e9
ShrutiSuman08 <shruti071203@gmail.com>
2026-09-09 00:10:43 +0530

    Fix HuggingFace dependencies and chatbot deployment

--- app.py
+++ app.py
@@ -13,8 +13,8 @@ app = Flask(__name__)
 
 load_dotenv()
 
-PINECONE_API_KEY = os.environ.get('PINECONE_API_KEY')
-GROQ_API_KEY = os.environ.get('GROQ_API_KEY')
+PINECONE_API_KEY = os.environ.get("PINECONE_API_KEY")
+GROQ_API_KEY = os.environ.get("GROQ_API_KEY")
 
 os.environ["PINECONE_API_KEY"] = PINECONE_API_KEY
 os.environ["GROQ_API_KEY"] = GROQ_API_KEY
@@ -22,16 +22,23 @@ os.environ["GROQ_API_KEY"] = GROQ_API_KEY
 
 embeddings = download_hugging_face_embeddings()
 
-index_name = "medical-chatbot" 
+index_name = "medical-chatbot"
+
 docsearch = PineconeVectorStore.from_existing_index(
     index_name=index_name,
     embedding=embeddings
 )
 
+retriever = docsearch.as_retriever(
+    search_type="similarity",
+    search_kwargs={"k": 3}
+)
 
-retriever = docsearch.as_retriever(search_type="similarity", search_kwargs={"k":3})
+chatModel = ChatGroq(
+    model="openai/gpt-oss-20b",
+    temperature=0.4
+)
 
-chatModel = ChatGroq(model="llama-3.3-70b-versatile", temperature=0.4)
 prompt = ChatPromptTemplate.from_messages(
     [
         ("system", system_prompt),
@@ -39,27 +46,38 @@ prompt = ChatPromptTemplate.from_messages(
     ]
 )
 
-question_answer_chain = create_stuff_documents_chain(chatModel, prompt)
-rag_chain = create_retrieval_chain(retriever, question_answer_chain)
+question_answer_chain = create_stuff_documents_chain(
+    chatModel,
+    prompt
+)
 
+rag_chain = create_retrieval_chain(
+    retriever,
+    question_answer_chain
+)
 
 
 @app.route("/")
 def index():
-    return render_template('chat.html')
-
+    return render_template("chat.html")
 
 
 @app.route("/get", methods=["GET", "POST"])
 def chat():
     msg = request.form["msg"]
-    input = msg
-    print(input)
+
+    print(msg)
+
     response = rag_chain.invoke({"input": msg})
-    print("Response : ", response["answer"])
+
+    print("Response:", response["answer"])
+
     return str(response["answer"])
 
 
-
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port= 8080, debug= True)
+if __name__ == "__main__":
+    app.run(
+        host="0.0.0.0",
+        port=8080,
+        debug=True
+    )
